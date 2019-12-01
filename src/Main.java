ccf925bf2881005c20c3eb7738135749e29e1d0d
BollingR0lling <43376259+BollingR0lling@users.noreply.github.com>
2019-12-01 13:34:19 +0500

    Rework some

--- src/Main.java
+++ src/Main.java
@@ -2,30 +2,21 @@ public class Main {
     public static void main(String[] args) {
 
         Figure circle = new Circle(6);
-        Figure square = new Square(7 , 8);
-        Figure equilateralTriangle = new EquilateralTriangle(5 , 6);
+        
+        Visitor figureDrawer = new DrawFigure();
+        Visitor areaGetter = new AreaGet();
+        Visitor showName = new ShowFigureName();
 
-        Visitor squareDrawer = new SquareDrawer();
-        Visitor circleDrawer = new CircleDrawer();
-        Visitor equilateralTriangleDrawer = new EquilateralTriangleDrawer();
-
-        circle.accept(circleDrawer);//it's working
-        circle.accept(squareDrawer);//doesnt workings
+        circle.accept(figureDrawer);
+        circle.accept(showName);
+        circle.accept(areaGetter);
 
     }
 
     interface Visitor {
-        void Draw(Square square);
-        void ShowFigureName(Square square);
-        void GetArea(Square square);
-
-        void Draw(EquilateralTriangle triangle);
-        void ShowFigureName(EquilateralTriangle triangle);
-        void GetArea(EquilateralTriangle triangle);
-
-        void Draw(Circle circle);
-        void ShowFigureName(Circle circle);
-        void GetArea(Circle circle);
+        void visit(Circle figure);
+        void visit(EquilateralTriangle figure);
+        void visit(Square figure);
     }
 
     interface Figure {
@@ -46,9 +37,7 @@ public class Main {
 
         @Override
         public void accept(Visitor visitor) {
-            visitor.Draw(this);
-            visitor.ShowFigureName(this);
-            visitor.GetArea(this);
+            visitor.visit(this);
         }
     }
 
@@ -65,9 +54,7 @@ public class Main {
         }
         @Override
         public void accept(Visitor visitor) {
-            visitor.Draw(this);
-            visitor.ShowFigureName(this);
-            visitor.GetArea(this);
+            visitor.visit(this);
         }
     }
 
@@ -82,94 +69,63 @@ public class Main {
         }
         @Override
         public void accept(Visitor visitor) {
-            visitor.Draw(this);
-            visitor.ShowFigureName(this);
-            visitor.GetArea(this);
+            visitor.visit(this);
         }
     }
 
-    static class SquareDrawer implements Visitor {
+    static class DrawFigure implements Visitor {
+
 
         @Override
-        public void Draw(Square square) {
-            System.out.println("Нарисована фигура со сторонами " + square.width + " " + square.height);
+        public void visit(Circle figure) {
+            System.out.println("Нарисована фигура с радиусом " + figure.radius);
         }
-        @Override
-        public void ShowFigureName(Square square) {
-            System.out.println("Нарисован квадрат");
-        }
-        @Override
-        public void GetArea(Square square) {
 
-            System.out.println("Площадь квадрата = " + square.getArea());
+        @Override
+        public void visit(EquilateralTriangle figure) {
+            System.out.println("Нарисована фигура с высотой " + figure.height + " и основанием " + figure.side);
         }
+
         @Override
-        public void GetArea(EquilateralTriangle triangle) { }
-        @Override
-        public void Draw(EquilateralTriangle triangle) { }
-        @Override
-        public void ShowFigureName(EquilateralTriangle triangle) { }
-        @Override
-        public void GetArea(Circle circle) { }
-        @Override
-        public void Draw(Circle circle) { }
-        @Override
-        public void ShowFigureName(Circle circle) { }
+        public void visit(Square figure) {
+            System.out.println("Нарисована фигура со сторонами " + figure.height + " и " + figure.width);
+        }
     }
+    static class ShowFigureName implements Visitor {
 
-    static class EquilateralTriangleDrawer implements Visitor {
-
-        @Override
-        public void GetArea(Square square) { }
-        @Override
-        public void Draw(Square square) { }
-        @Override
-        public void ShowFigureName(Square square) { }
-        @Override
-        public void Draw(EquilateralTriangle triangle) {
-            System.out.println("Нарисована фигура с высотой " + triangle.height + " и основанием " + triangle.height);
-        }
-        @Override
-        public void ShowFigureName(EquilateralTriangle triangle) {
-            System.out.println("Нарисован треугольник");
-        }
-        @Override
-        public void GetArea(EquilateralTriangle triangle) {
-            System.out.println("Площадь треугольника равна = " + triangle.getArea());
-        }
-        @Override
-        public void GetArea(Circle circle) { }
-        @Override
-        public void Draw(Circle circle) { }
-        @Override
-        public void ShowFigureName(Circle circle) { }
-    }
-
-    static class CircleDrawer implements Visitor {
 
         @Override
-        public void GetArea(Square square) { }
-        @Override
-        public void Draw(Square square) { }
-        @Override
-        public void ShowFigureName(Square square) { }
-        @Override
-        public void GetArea(EquilateralTriangle triangle) { }
-        @Override
-        public void Draw(EquilateralTriangle triangle) { }
-        @Override
-        public void ShowFigureName(EquilateralTriangle triangle) { }
-        @Override
-        public void Draw(Circle circle) {
-            System.out.println("Пользователь фигуру с радиусом " + circle.radius);
-        }
-        @Override
-        public void ShowFigureName(Circle circle) {
+        public void visit(Circle figure) {
             System.out.println("Данная фигура называется круг");
         }
+
         @Override
-        public void GetArea(Circle circle) {
-            System.out.println("Площадь круга = " + circle.getArea());
+        public void visit(EquilateralTriangle figure) {
+            System.out.println("Данная фигура называется треугольник");
+        }
+
+        @Override
+        public void visit(Square figure) {
+            System.out.println("Данная фигура называется прямоугольник");
+        }
+    }
+
+    static class AreaGet implements Visitor {
+
+
+        @Override
+        public void visit(Circle figure) {
+            System.out.println("Площадь данной фигуры равна " + figure.getArea());
+        }
+
+        @Override
+        public void visit(EquilateralTriangle figure) {
+            System.out.println("Площадь данной фигуры равна " + figure.getArea());
+        }
+
+        @Override
+        public void visit(Square figure) {
+            System.out.println("Площадь данной фигуры равна " + figure.getArea());
         }
     }
 }
